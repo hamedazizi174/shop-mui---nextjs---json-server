70b2227a2675dc247b4e23d539a9d426f3e4ba21
hamed <hamedazizi1374@gmail.com>
2024-06-10 19:19:34 +0330

    feat:create loading for categories

---
+++ pages/products/index.tsx
@@ -0,0 +1,5 @@
+import ProductsTemplate from "@/src/components/templates/products/productsTemplate";
+
+export default function ProductsPage() {
+  return <ProductsTemplate />;
+}
--- src/components/templates/home/Categories/Categories.tsx
+++ src/components/templates/home/Categories/Categories.tsx
@@ -5,24 +5,30 @@ import CategoryCard from "./CategoryCard/CategoryCard";
 
 export default function Categories() {
   const [categories, setCategories] = useState<any>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     getCategoris().then((data) => setCategories(data));
+    setIsLoading(false);
   }, []);
   return (
     <Box p="20px">
       <Typography variant="h4" m="20px">
         Shop Our Top Categories
       </Typography>
-      <Stack direction="row" justifyContent="space-between" m="20px">
-        {categories.map((category: any) => (
-          <CategoryCard
-            url={category.image}
-            title={category.title}
-            key={category.id}
-          />
-        ))}
-      </Stack>
+      {isLoading ? (
+        <Typography variant="h4">Loading...</Typography>
+      ) : (
+        <Stack direction="row" justifyContent="space-between" m="20px">
+          {categories.map((category: any) => (
+            <CategoryCard
+              url={category.image}
+              title={category.title}
+              key={category.id}
+            />
+          ))}
+        </Stack>
+      )}
     </Box>
   );
 }
